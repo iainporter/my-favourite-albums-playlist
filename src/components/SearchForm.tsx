0c26a31bd555d2fccea0fa1e1ffa74d33644a202
iainporter <changeant@gmail.com>
2024-11-14 08:54:56 +0000

    change search button

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -127,7 +127,7 @@ export default function SearchForm({ accessToken }: SearchFormProps) {
           type="submit"
           className="w-full px-4 py-2 bg-spotify-green text-white rounded-full hover:bg-green-600 transition-colors duration-200"
         >
-          Search
+          Search Spotify
         </button>
       </form>
 
