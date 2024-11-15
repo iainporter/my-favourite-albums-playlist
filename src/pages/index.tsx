e72f973784b0409c952c47ff055067587e0f49b3
iainporter <changeant@gmail.com>
2024-11-15 18:22:33 +0000

    Added Publications component to the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import PlaylistManager from '../components/PlaylistManager';
 import FavoriteAlbums from '../components/FavoriteAlbums';
+import Publications from '../components/Publications';
 
 export default function Home() {
   const router = useRouter();
